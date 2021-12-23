c7c6c9316095d77773d8a4004d055ff894d62a11
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2021-12-23 13:56:29 +0300

    Commit changes to red team RedTeamAgent
    Change values to be generic and not identify my domain etc.

--- RedTeamAgent/internet.cpp
+++ RedTeamAgent/internet.cpp
@@ -1,6 +1,6 @@
 #include "http.h"
 
-LPCSTR UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36";
+LPCSTR UserAgent = ""; // Insert user agent here
 
  HINTERNET SendRequest(LPCSTR Domain, int Port, LPCSTR URI, LPCSTR Verb, const char* Params)
 {
