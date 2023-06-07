6b10ee45611783a13748292e526e84c54e81b996
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2023-06-07 14:41:49 +0200

    Initial commit for new project.
    Commit includes ssh_C2.cpp file.

---
+++ SSH_Command_and_Control/ssh_C2.cpp
@@ -0,0 +1,10 @@
+// SSH_C&C.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// You will need to install libssh https://www.libssh.org/get-it/
+#include <libssh/libssh.h>
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
