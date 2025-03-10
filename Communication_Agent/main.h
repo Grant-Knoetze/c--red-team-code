2443b835d9899e34a9afdaf8def0b642440da157
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2025-03-10 21:38:24 +0200

    Rename directory.
    Rename directory and restore files.

---
+++ Communication_Agent/main.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <Windows.h>
+#include <WinInet.h>
+#include <regex>
+#include "base64.h"
+#include "internet.h"
+#include "json.hpp"
+#include "execute.h"
+
+using namespace std;
+using namespace json;
